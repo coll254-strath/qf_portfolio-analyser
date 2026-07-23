45ac2b5748d517e9e436af9e6098ec5b96f6c20e
Collins <149684399+coll254-strath@users.noreply.github.com>
2026-07-24 00:04:39 +0100

    Update setup instructions in equalWeight.py

--- equalWeight.py
+++ equalWeight.py
@@ -1,4 +1,4 @@
-Setup required before running :
+""" Setup required before running :
     !apt-get update -qq
     !apt-get install -y -qq libcairo2 libpango-1.0-0 libpangocairo-1.0-0 \
         libgdk-pixbuf2.0-0 shared-mime-info poppler-utils
